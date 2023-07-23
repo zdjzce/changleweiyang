f51aa896ee6955062913b166c475bf055b57bcde
zdjzce923 <493904954@qq.com>
2023-07-23 22:01:27 +0800

    style: add button

--- packages/changleweiyang/global.d.ts
+++ packages/changleweiyang/global.d.ts
@@ -1,5 +1,10 @@
 declare module '*.vue' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent;
-  export default component;
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent
+  export default component
+}
+
+declare module '*.scss' {
+  const content: Record<string, string>
+  export default content
 }
